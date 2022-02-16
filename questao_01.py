022d964ab2a515a5a8344ebc5c1ee25541728e5a
Meyre Anusia <89615679+meyreanusia@users.noreply.github.com>
2022-02-16 16:04:57 -0300

    Update questao_01.py

--- questao_01.py
+++ questao_01.py
@@ -1,3 +1,6 @@
+''''Escreva um algoritmo que mostre na tela uma escada de tamanho n utilizando o caractere * e espaços. 
+A base e altura da escada devem ser iguais ao valor de n. A última linha não deve conter nenhum espaço.'''
+
 def escada (n):
     for i in range(n +1):
         print("*" * i)
