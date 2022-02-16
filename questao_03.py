f301d143be5233dcd32df272f7a1fb9acaf88fa0
Meyre Anusia <89615679+meyreanusia@users.noreply.github.com>
2022-02-16 16:11:44 -0300

    Update questao_03.py

--- questao_03.py
+++ questao_03.py
@@ -1,8 +1,8 @@
-import itertools
+import itertools #o método combinations para fazer combinações.
 
 senha = input(str("Digite a senha: "))
 lista = list(senha)
 
-x = list(itertools.combinations(lista, 2))
+x = list(itertools.combinations(lista, r=2)) #r - parametro para permutaçao com 2 elementos.
 print(x)
 print(len(x))
